7e8188f4b5f9be7a2f76261ea5b571a7984848d5
Lucas C <lcosier@ethz.ch>
2023-04-01 18:38:27 +0200

    2b speedup plot

--- Part2/2b_speedup.py
+++ Part2/2b_speedup.py
@@ -14,13 +14,26 @@ plt.rcParams.update({
     "errorbar.capsize": 4
 })
 
+COLORS = {
+    r'\emph{black scholes}': 'darkblue',
+    r'\emph{canneal}': 'royalblue', # suggest something yellow-ish
+    r'\emph{dedup}': 'maroon',
+    r'\emph{ferret}': 'darkgreen',
+    r'\emph{freqmine}': 'darkorange',
+    r'\emph{radix}': 'orangered',
+    r'\emph{vips}': 'purple'
+}
+
+
 # ------------------ FUNCTIONS ------------------
 
-def create_plot(xticks, xtick_labels, speedup, name):
+def create_plot(xticks, xtick_labels, response_times, name):
     # generate plot
     plt.figure(figsize=(12, 12))
 
-    plt.plot(xticks, speedup, marker='o', ms=7, c='goldenrod', linewidth=2.2)
+    for i in range(len(response_times)):
+        plt.plot(xticks, response_times[i], marker='o', markersize=10, color=COLORS[list(COLORS.keys())[i]],
+                 label=list(COLORS.keys())[i])
 
     # move title and labeling
     ax = plt.gca()
@@ -28,7 +41,7 @@ def create_plot(xticks, xtick_labels, speedup, name):
     plt.ylabel(r'\bf{Speedup}', rotation=0, size=15)
     ax.yaxis.set_label_coords(0.0, 1.03)
     plt.title(r"\bf{Measured Parsec Parallel Speedup}", loc='left', pad=60, size=18)
-    # plt.legend(prop={'size': 14.5}, loc='upper left')
+    plt.legend(prop={'size': 14.5}, loc='upper left')
     plt.tight_layout()
 
     # axes adjustments
@@ -51,17 +64,24 @@ def create_plot(xticks, xtick_labels, speedup, name):
 
 
 def compute_normalized_speedup(response_times):
-    normalized = [1]
-    for i in range(1, len(response_times)):
-        normalized.append(response_times[0] / response_times[i])
+    normalized = np.zeros_like(response_times)
+    for i in range(len(response_times)):
+        normalized[i] = response_times[i][0] / response_times[i]
     return normalized
 
 
 def main():
-    response_times = [853.0, 442.0, 270.5, 165.9]
+    black_scholes = np.array([125.102, 72.709, 47.359, 36.309], dtype=np.float64)
+    canneal = np.array([252.449, 188.145, 131.056, 107.335], dtype=np.float64)
+    dedup = np.array([20.522, 13.405, 11.784, 11.503], dtype=np.float64)
+    ferret = np.array([320.171, 163.943, 95.431, 81.764], dtype=np.float64)
+    freqmine = np.array([490.138, 252.886, 127.461, 101.923], dtype=np.float64)
+    radix = np.array([53.043, 30.083, 15.014, 9.585], dtype=np.float64)
+    vips = np.array([99.068, 52.883, 26.044, 22.201], dtype=np.float64)
     xticks = [1, 2, 4, 8]
-    xtick_labels = [1, 2, 4, 8]
+    # xtick_labels = [1, 2, 4, 8]
     xtick_labels = [r'$2^0$', r'$2^1$', r'$2^2$', r'$2^3$']
+    response_times = np.stack((black_scholes, canneal, dedup, ferret, freqmine, radix, vips), axis=0)
     response_times = compute_normalized_speedup(response_times)
     print(response_times)
     create_plot(xticks, xtick_labels, response_times, 'speedup')
