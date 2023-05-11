7ff5eddd566ef1e3267d30d2dff2ea7b9b3c4fdf
asi345 <inanata15@gmail.com>
2023-05-11 21:44:51 +0200

    finished part4 q1

--- Part4/Q1/cont_memcached.sh
+++ Part4/Q1/cont_memcached.sh
@@ -1,6 +1,6 @@
-sudo sed -i -e "s/-p 11211 -t 2/-p 11211 -t 2/" /etc/memcached.conf
+sudo sed -i -e "s/-p 11211 -t 1/-p 11211 -t 1/" /etc/memcached.conf
 sudo systemctl restart memcached
 sleep 10
-sudo taskset -acp 0,1 $(pgrep memcached)
+sudo taskset -acp 1 $(pgrep memcached)
 sleep 10
 sudo systemctl status memcached
--- Part4/Q1/install_memcached.sh
+++ Part4/Q1/install_memcached.sh
@@ -4,7 +4,7 @@ sudo systemctl status memcached
 source memcached_int_ip.txt
 sudo sed -i -e "s/-m 64/-m 1024/" /etc/memcached.conf
 sudo sed -i -e "s/-l 127.0.0.1/-l $MEMCACHED_INT_IP/" /etc/memcached.conf
-sudo sed -i -e "s/-p 11211/-p 11211 -t s1/" /etc/memcached.conf
+sudo sed -i -e "s/-p 11211/-p 11211 -t 1/" /etc/memcached.conf
 sudo systemctl restart memcached
 sleep 10
 sudo taskset -acp 0 $(pgrep memcached)
