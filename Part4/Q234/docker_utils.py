f79022dd236a5767355fffc75c3609b1a5df29d2
tolgakerimoglu <tolgakerimoglu@outlook.com>
2023-05-12 14:43:31 +0200

    initial docker handler

---
+++ Part4/Q234/docker_utils.py
@@ -0,0 +1,99 @@
+import docker
+from scheduler_logger import SchedulerLogger, Job
+import psutil
+from jobs import images, threads, cores
+
+class ContainerHandler:
+    def __init__(self):
+        self.client = docker.from_env()
+        self.logger = SchedulerLogger()
+        self.containers = []
+
+    def create_container(self, name, cpu_period=100000, cpu_quota=100000):
+        config = {
+            "name": name,
+            "cpuset_cpus": cores,
+            "image": images[name],
+            "command": f"./bin/parsecmgmt -a run -p {cores[name]} -i native -n {threads[name]}",
+            "detach": True,
+            "auto_remove": False
+        }
+        container = self.client.containers.run(**config)
+        self.logger.job_start(Job(container.name), initial_cores=cores.split(","), initial_threads=num_threads)
+        container.reload()
+        return container
+
+    def remove_if_exited(self, container):
+        if container == None:
+            return False 
+        
+        container.reload()
+        if container.status == "exited":
+            try:
+                container.remove()
+                self.logger.job_end(Job(container.name))
+                return True
+            except:
+                container.remove(force=True)
+                self.logger.job_end(Job(container.name))
+                return True
+            
+        return False
+
+    def remove(self, container):
+        if container == None:
+            return None
+        
+        container.reload()
+        try:
+            container.remove()
+            self.logger.job_end(Job(container.name))
+            return True
+        except:
+            container.remove(force=True)
+            self.logger.job_end(Job(container.name))
+            return True
+
+        
+    def pause(self, container):
+        if container == None:
+            return None 
+        
+        container.reload()
+        if container.status == "running":
+            container.pause()
+            self.logger.job_pause(Job(container.name))
+            return True 
+        
+        return False
+    
+    def unpause(self, container):
+        if container == None:
+            return None 
+        
+        container.reload()
+        if container.status == "paused":
+            container.unpause()
+            self.logger.job_unpaused(Job(container.name))
+            return True
+        elif container.status == "created":
+            container.start()
+            self.logger.job_unpause(Job(container.name))
+            return True 
+        
+        return False
+    
+    def update_cpu_shares(self, container, shares): # shares between 0-400
+        if container == None:
+            return None 
+        
+        container.reload()
+        container.update(cpu_quoata = shares * 100000)
+
+
+
+if __name__ == "__main__":
+    handler = ContainerHandler()
+    cont = handler.create_container("dedup")
+    while not handler.remove_if_exited(cont):
+        print(psutil.cpu_percent(interval=0.1, percpu=True))
---
+++ Part4/Q234/jobs.py
@@ -0,0 +1,29 @@
+images = {
+    "dedup": "anakli/cca:parsec_dedup",
+    "blackscholes": "anakli/cca:parsec_blackscholes",
+    "canneal": "anakli/cca:parsec_canneal",
+    "ferret": "anakli/cca:parsec_ferret",
+    "freqmine": "anakli/cca:parsec_freqmine",
+    "radix": "anakli/cca:splash2x_radix",
+    "vips": "anakli/cca:parsec_vips"
+}
+
+threads = {
+    "dedup": 2,
+    "blackscholes": 2,
+    "canneal": 2,
+    "ferret": 2,
+    "freqmine": 2,
+    "radix": 2,
+    "vips": 2
+}
+
+cores = {
+    "dedup": "0,1,2,3",
+    "blackscholes": "0,1,2,3",
+    "canneal": "0,1,2,3",
+    "ferret": "0,1,2,3",
+    "freqmine": "0,1,2,3",
+    "radix": "0,1,2,3",
+    "vips": "0,1,2,3"
+}
